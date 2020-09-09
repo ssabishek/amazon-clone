f639b35554c1402bec49c61cb8c4936a570619aa
Abishek S S <ssabishekme@gmail.com>
2020-09-09 12:21:57 +0530

    Added Header and Home page functionality

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Product from "./Product";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          className="home__image"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img20/Wireless/SamsungM/M51/8thSept_GW/P38983965_IN_WLME_SamsungGalaxy_M51_New_Launch_DesktopTallHero_2_1500x600._CB405103024_.jpg"
+          alt="hello"
+        />
+        <div className="home__row">
+          <Product
+            title="The lean startup"
+            price={29.99}
+            image="https://images-eu.ssl-images-amazon.com/images/G/31/img18/Ebooks/202007/pc_low._SY304_CB409836418_.jpg"
+            rating={5}
+          />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+          <Product />
+          <Product />
+        </div>
+        <div className="home__row">
+          <Product />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
